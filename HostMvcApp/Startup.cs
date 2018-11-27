b25b732eef3391d4c7caea01b58dd657dc995a9a
damienbod <damien_bod@hotmail.com>
2018-11-27 18:00:29 +0100

    Adding routes

--- HostMvcApp/Startup.cs
+++ HostMvcApp/Startup.cs
@@ -64,6 +64,10 @@ namespace HostMvcApp
                   name: "areas",
                   template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
